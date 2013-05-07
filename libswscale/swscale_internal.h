f394d3226789597a7213505631fac24a1970ce0a
Dean Herbert <pe@ppy.sh>
2013-05-08 07:02:40 +0800

    swscale update?

--- libswscale/swscale_internal.h
+++ libswscale/swscale_internal.h
@@ -23,7 +23,7 @@
 
 #include "config.h"
 
-#ifdef HAVE_ALTIVEC_H
+#if HAVE_ALTIVEC_H
 #include <altivec.h>
 #endif
 
@@ -42,7 +42,7 @@
 #define ALT32_CORR   1
 #endif
 
-#ifdef ARCH_X86_64
+#if ARCH_X86_64
 #   define APCK_PTR2 8
 #   define APCK_COEF 16
 #   define APCK_SIZE 24
@@ -52,8 +52,11 @@
 #   define APCK_SIZE 16
 #endif
 
-typedef int (*SwsFunc)(struct SwsContext *context, uint8_t* src[], int srcStride[], int srcSliceY,
-             int srcSliceH, uint8_t* dst[], int dstStride[]);
+struct SwsContext;
+
+typedef int (*SwsFunc)(struct SwsContext *context, uint8_t* src[],
+                       int srcStride[], int srcSliceY, int srcSliceH,
+                       uint8_t* dst[], int dstStride[]);
 
 /* This struct should be aligned on at least a 32-byte boundary. */
 typedef struct SwsContext{
@@ -71,7 +74,7 @@ typedef struct SwsContext{
     int chrSrcW, chrSrcH, chrDstW, chrDstH;
     int lumXInc, chrXInc;
     int lumYInc, chrYInc;
-    int dstFormat, srcFormat;               ///< format 4:2:0 type is always YV12
+    enum PixelFormat dstFormat, srcFormat;  ///< format 4:2:0 type is always YV12
     int origDstFormat, origSrcFormat;       ///< format
     int chrSrcHSubSample, chrSrcVSubSample;
     int chrIntHSubSample, chrIntVSubSample;
@@ -80,8 +83,12 @@ typedef struct SwsContext{
     int sliceDir;
     double param[2];
 
+    uint32_t pal_yuv[256];
+    uint32_t pal_rgb[256];
+
     int16_t **lumPixBuf;
     int16_t **chrPixBuf;
+    int16_t **alpPixBuf;
     int16_t *hLumFilter;
     int16_t *hLumFilterPos;
     int16_t *hChrFilter;
@@ -126,6 +133,12 @@ typedef struct SwsContext{
     int srcColorspaceTable[4];
     int dstColorspaceTable[4];
     int srcRange, dstRange;
+    int yuv2rgb_y_offset;
+    int yuv2rgb_y_coeff;
+    int yuv2rgb_v2r_coeff;
+    int yuv2rgb_v2g_coeff;
+    int yuv2rgb_u2g_coeff;
+    int yuv2rgb_u2b_coeff;
 
 #define RED_DITHER            "0*8"
 #define GREEN_DITHER          "1*8"
@@ -145,6 +158,8 @@ typedef struct SwsContext{
 #define VROUNDER_OFFSET       "11*8+4*4*256*2+16"
 #define U_TEMP                "11*8+4*4*256*2+24"
 #define V_TEMP                "11*8+4*4*256*2+32"
+#define Y_TEMP                "11*8+4*4*256*2+40"
+#define ALP_MMX_FILTER_OFFSET "11*8+4*4*256*2+48"
 
     uint64_t redDither   __attribute__((aligned(8)));
     uint64_t greenDither __attribute__((aligned(8)));
@@ -165,9 +180,10 @@ typedef struct SwsContext{
     uint64_t vRounder     __attribute__((aligned(8)));
     uint64_t u_temp       __attribute__((aligned(8)));
     uint64_t v_temp       __attribute__((aligned(8)));
+    uint64_t y_temp       __attribute__((aligned(8)));
+    int32_t  alpMmxFilter[4*MAX_FILTER_SIZE];
 
-#ifdef HAVE_ALTIVEC
-
+#if HAVE_ALTIVEC
   vector signed short   CY;
   vector signed short   CRV;
   vector signed short   CBU;
@@ -176,11 +192,9 @@ typedef struct SwsContext{
   vector signed short   OY;
   vector unsigned short CSHIFT;
   vector signed short   *vYCoeffsBank, *vCCoeffsBank;
-
 #endif
 
-
-#ifdef ARCH_BFIN
+#if ARCH_BFIN
     uint32_t oy           __attribute__((aligned(4)));
     uint32_t oc           __attribute__((aligned(4)));
     uint32_t zero         __attribute__((aligned(4)));
@@ -194,22 +208,85 @@ typedef struct SwsContext{
     uint32_t gmask        __attribute__((aligned(4)));
 #endif
 
-#ifdef HAVE_VIS
+#if HAVE_VIS
     uint64_t sparc_coeffs[10] __attribute__((aligned(8)));
 #endif
+
+    /* function pointers for swScale() */
+    void (*yuv2nv12X  )(struct SwsContext *c,
+                        const int16_t *lumFilter, const int16_t **lumSrc, int lumFilterSize,
+                        const int16_t *chrFilter, const int16_t **chrSrc, int chrFilterSize,
+                        uint8_t *dest, uint8_t *uDest,
+                        int dstW, int chrDstW, int dstFormat);
+    void (*yuv2yuv1   )(struct SwsContext *c,
+                        const int16_t *lumSrc, const int16_t *chrSrc, const int16_t *alpSrc,
+                        uint8_t *dest,
+                        uint8_t *uDest, uint8_t *vDest, uint8_t *aDest,
+                        long dstW, long chrDstW);
+    void (*yuv2yuvX   )(struct SwsContext *c,
+                        const int16_t *lumFilter, const int16_t **lumSrc, int lumFilterSize,
+                        const int16_t *chrFilter, const int16_t **chrSrc, int chrFilterSize,
+                        const int16_t **alpSrc,
+                        uint8_t *dest,
+                        uint8_t *uDest, uint8_t *vDest, uint8_t *aDest,
+                        long dstW, long chrDstW);
+    void (*yuv2packed1)(struct SwsContext *c,
+                        const uint16_t *buf0,
+                        const uint16_t *uvbuf0, const uint16_t *uvbuf1,
+                        const uint16_t *abuf0,
+                        uint8_t *dest,
+                        int dstW, int uvalpha, int dstFormat, int flags, int y);
+    void (*yuv2packed2)(struct SwsContext *c,
+                        const uint16_t *buf0, const uint16_t *buf1,
+                        const uint16_t *uvbuf0, const uint16_t *uvbuf1,
+                        const uint16_t *abuf0, const uint16_t *abuf1,
+                        uint8_t *dest,
+                        int dstW, int yalpha, int uvalpha, int y);
+    void (*yuv2packedX)(struct SwsContext *c,
+                        const int16_t *lumFilter, const int16_t **lumSrc, int lumFilterSize,
+                        const int16_t *chrFilter, const int16_t **chrSrc, int chrFilterSize,
+                        const int16_t **alpSrc, uint8_t *dest,
+                        long dstW, long dstY);
+
+    void (*hyscale_internal)(uint8_t *dst, const uint8_t *src,
+                             long width, uint32_t *pal);
+    void (*hascale_internal)(uint8_t *dst, const uint8_t *src,
+                             long width, uint32_t *pal);
+    void (*hcscale_internal)(uint8_t *dstU, uint8_t *dstV,
+                             const uint8_t *src1, const uint8_t *src2,
+                             long width, uint32_t *pal);
+    void (*hyscale_fast)(struct SwsContext *c,
+                         int16_t *dst, int dstWidth,
+                         const uint8_t *src, int srcW, int xInc);
+    void (*hcscale_fast)(struct SwsContext *c,
+                         int16_t *dst, int dstWidth,
+                         const uint8_t *src1, const uint8_t *src2,
+                         int srcW, int xInc);
+
+    void (*hScale)(int16_t *dst, int dstW, const uint8_t *src, int srcW,
+                   int xInc, const int16_t *filter, const int16_t *filterPos,
+                   long filterSize);
 
 } SwsContext;
 //FIXME check init (where 0)
 
-SwsFunc yuv2rgb_get_func_ptr (SwsContext *c);
-int yuv2rgb_c_init_tables (SwsContext *c, const int inv_table[4], int fullRange, int brightness, int contrast, int saturation);
+SwsFunc ff_yuv2rgb_get_func_ptr(SwsContext *c);
+int ff_yuv2rgb_c_init_tables(SwsContext *c, const int inv_table[4],
+                             int fullRange, int brightness,
+                             int contrast, int saturation);
 
-void yuv2rgb_altivec_init_tables (SwsContext *c, const int inv_table[4],int brightness,int contrast, int saturation);
-SwsFunc yuv2rgb_init_altivec (SwsContext *c);
-void altivec_yuv2packedX (SwsContext *c,
-                          int16_t *lumFilter, int16_t **lumSrc, int lumFilterSize,
-                          int16_t *chrFilter, int16_t **chrSrc, int chrFilterSize,
-                          uint8_t *dest, int dstW, int dstY);
+void ff_yuv2rgb_init_tables_altivec(SwsContext *c, const int inv_table[4],
+                                    int brightness, int contrast, int saturation);
+SwsFunc ff_yuv2rgb_init_mmx(SwsContext *c);
+SwsFunc ff_yuv2rgb_init_vis(SwsContext *c);
+SwsFunc ff_yuv2rgb_init_mlib(SwsContext *c);
+SwsFunc ff_yuv2rgb_init_altivec(SwsContext *c);
+SwsFunc ff_yuv2rgb_get_func_ptr_bfin(SwsContext *c);
+void ff_bfin_get_unscaled_swscale(SwsContext *c);
+void ff_yuv2packedX_altivec(SwsContext *c,
+                            int16_t *lumFilter, int16_t **lumSrc, int lumFilterSize,
+                            int16_t *chrFilter, int16_t **chrSrc, int chrFilterSize,
+                            uint8_t *dest, int dstW, int dstY);
 
 const char *sws_format_name(int format);
 
@@ -217,6 +294,7 @@ const char *sws_format_name(int format);
 #define isPlanarYUV(x)  (           \
            (x)==PIX_FMT_YUV410P     \
         || (x)==PIX_FMT_YUV420P     \
+        || (x)==PIX_FMT_YUVA420P    \
         || (x)==PIX_FMT_YUV411P     \
         || (x)==PIX_FMT_YUV422P     \
         || (x)==PIX_FMT_YUV444P     \
@@ -248,6 +326,7 @@ const char *sws_format_name(int format);
         || (x)==PIX_FMT_RGB4        \
         || (x)==PIX_FMT_RGB4_BYTE   \
         || (x)==PIX_FMT_MONOBLACK   \
+        || (x)==PIX_FMT_MONOWHITE   \
     )
 #define isBGR(x)        (           \
            (x)==PIX_FMT_BGR32       \
@@ -259,6 +338,14 @@ const char *sws_format_name(int format);
         || (x)==PIX_FMT_BGR4        \
         || (x)==PIX_FMT_BGR4_BYTE   \
         || (x)==PIX_FMT_MONOBLACK   \
+        || (x)==PIX_FMT_MONOWHITE   \
+    )
+#define isALPHA(x)      (           \
+           (x)==PIX_FMT_BGR32       \
+        || (x)==PIX_FMT_BGR32_1     \
+        || (x)==PIX_FMT_RGB32       \
+        || (x)==PIX_FMT_RGB32_1     \
+        || (x)==PIX_FMT_YUVA420P    \
     )
 
 static inline int fmt_depth(int fmt)
@@ -289,14 +376,15 @@ static inline int fmt_depth(int fmt)
         case PIX_FMT_RGB4_BYTE:
             return 4;
         case PIX_FMT_MONOBLACK:
+        case PIX_FMT_MONOWHITE:
             return 1;
         default:
             return 0;
     }
 }
 
-extern const DECLARE_ALIGNED(8, uint64_t, ff_dither4[2]);
-extern const DECLARE_ALIGNED(8, uint64_t, ff_dither8[2]);
+extern const uint64_t ff_dither4[2];
+extern const uint64_t ff_dither8[2];
 
 extern const AVClass sws_context_class;
 
